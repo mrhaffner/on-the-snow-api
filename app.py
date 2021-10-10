ef75843f933680d6258ba1fb323bb863254e8ab3
mrhaffner <mrhaffner13@gmail.com>
2021-10-09 19:25:28 -0500

    feat: added a route to get all resort names

--- app.py
+++ app.py
@@ -9,13 +9,17 @@ db = SQLAlchemy(app)
 
 db.Model.metadata.reflect(db.engine)
 
+@app.route("/resorts", methods=["GET"])
+def get_all_resort_names():
+    resorts = resort_controller.get_all_resort_names()
+    return jsonify(resorts)
 
 @app.route("/resort/<id>", methods=["GET"])
 def get_resort_by_id(id):
     resort = resort_controller.get_resort_by_id(id)
     return jsonify(resort)
 
-@app.route("/resort/state/<state>", methods=["GET"]) #maybe use id instead?
+@app.route("/resort/state/<state>", methods=["GET"])
 def get_resort_names_by_state(state):
     resorts = resort_controller.get_resort_names_by_state(state)
     return jsonify(resorts)
